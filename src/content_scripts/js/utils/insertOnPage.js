db8ff5f43246c44b2e76a4bfee235a7d246a9f31
Luis Pacheco <lpachecoby@gmail.com>
2022-10-26 10:11:57 -0500

    Update insertOnPage.js

--- src/content_scripts/js/utils/insertOnPage.js
+++ src/content_scripts/js/utils/insertOnPage.js
@@ -1,17 +1,33 @@
 export class insertOnPage {
-  // Antes que el propio elemento.
+  /**
+   * Antes que el propio elemento.
+   * @param {*} object
+   * @param {*} html
+   */
   static beforebegin(object, html) {
     object.insertAdjacentHTML("beforebegin", html);
   }
-  //Justo dentro del elemento, antes de su primer elemento hijo.
+  /**
+   * Justo dentro del elemento, antes de su primer elemento hijo.
+   * @param {*} object
+   * @param {*} html
+   */
   static afterbegin(object, html) {
     object.insertAdjacentHTML("afterbegin", html);
   }
-  //Justo dentro del elemento, después de su último elemento hijo.
+  /**
+   * Justo dentro del elemento, después de su último elemento hijo.
+   * @param {*} object
+   * @param {*} html
+   */
   static beforeend(object, html) {
     object.insertAdjacentHTML("beforeend", html);
   }
-  //Después del propio elemento.
+  /**
+   * Después del propio elemento.
+   * @param {*} object
+   * @param {*} html
+   */
   static afterend(object, html) {
     object.insertAdjacentHTML("afterend", html);
   }
