3692c52783d0b2736f0eced860777a9f5bc70b0b
Luis Pacheco <lpachecoby@gmail.com>
2022-11-28 11:59:25 -0500

    Se revirtieron los cambios

---
+++ images/screenshots/instalacion/README.md
@@ -0,0 +1,11 @@
+## GUIA DE INSTALACIÓN
+
+<img src="https://github.com/lpachecob/Gladiatus-HealthyGame/blob/main/images/screenshots/instalacion/1.png?raw=true">
+<img src="https://github.com/lpachecob/Gladiatus-HealthyGame/blob/main/images/screenshots/instalacion/2.png?raw=true">
+<img src="https://github.com/lpachecob/Gladiatus-HealthyGame/blob/main/images/screenshots/instalacion/3.png?raw=true">
+<img src="https://github.com/lpachecob/Gladiatus-HealthyGame/blob/main/images/screenshots/instalacion/4.png?raw=true">
+<img src="https://github.com/lpachecob/Gladiatus-HealthyGame/blob/main/images/screenshots/instalacion/5.png?raw=true">
+<img src="https://github.com/lpachecob/Gladiatus-HealthyGame/blob/main/images/screenshots/instalacion/6.png?raw=true">
+<img src="https://github.com/lpachecob/Gladiatus-HealthyGame/blob/main/images/screenshots/instalacion/7.png?raw=true">
+<img src="https://github.com/lpachecob/Gladiatus-HealthyGame/blob/main/images/screenshots/instalacion/8.png?raw=true">
+<img src="https://github.com/lpachecob/Gladiatus-HealthyGame/blob/main/images/screenshots/instalacion/9.png?raw=true">
---
+++ src/content_scripts/js/Menu/Menu.js
@@ -0,0 +1,34 @@
+import { insertOnPage } from "../utils/insertOnPage.js";
+
+export class Menu {
+  static Dibujar() {
+    document.body.insertAdjacentHTML(
+      "afterbegin",
+      `
+            <button id="MenuOpen" class="btnMenu" hidden></button>
+            <div id="menuSidenav" class="menutools">
+                 <h1>Configuración</h1>
+                 <hr/>
+                 <a id="CloseMenu" href="#" class="closebtn">&times;</a>
+                 <div id="menuContent"></div>
+            </div>
+            `
+    );
+    let menuOpen = document.getElementById("MenuOpen");
+    menuOpen.addEventListener("click", Menu.openNav);
+    menuOpen.addEventListener("touchstart", Menu.openNav);
+    let closeMenu = document.getElementById("CloseMenu");
+    closeMenu.addEventListener("click", Menu.closeNav);
+  }
+  static openNav() {
+    document.getElementById("menuSidenav").style.display = "block";
+  }
+
+  static closeNav() {
+    document.getElementById("menuSidenav").style.display = "none";
+  }
+  static addConfig(html) {
+    html += "<hr/>";
+    insertOnPage.beforeend(document.getElementById("menuContent"), html);
+  }
+}
